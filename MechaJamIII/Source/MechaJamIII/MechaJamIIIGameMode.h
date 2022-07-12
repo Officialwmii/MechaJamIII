87785685274482f3d01c121d66da28b3d1e842dc
Officialwmii <Officialwaynemii@gmail.com>
2022-07-12 16:33:13 -0400

    Unreal Project Initialization

---
+++ MechaJamIII/Source/MechaJamIII/MechaJamIIIGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MechaJamIIIGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMechaJamIIIGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMechaJamIIIGameMode();
+};
+
+
+
